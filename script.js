cbade37a56b6346d6bd7ffc4443021ad378d4ee9
AbdoMohamad <abdomohmed768@gmail.com>
2024-10-23 15:21:43 +0300

    Added themes switch

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const header = document.querySelector(".header");
 const switch_inp = document.querySelector(".switch");
-
 const screen_inp = document.querySelector(".screen");
-
 const inputs_container = document.querySelector(".inputs");
 
 let theme = 1;
@@ -27,12 +24,135 @@ const resetNumbers = function () {
   firstNum = 0;
   secondNum = 0;
 };
+const resetColors = function (e) {
+  switch (e.target.value) {
+    case "del":
+    case "reset":
+      e.target.style.backgroundColor = `var(--th-${theme}-key-bg-alt)`;
+      break;
 
-switch_inp.addEventListener("mousedown", function () {
-  switch_inp.firstElementChild.style.backgroundColor = `var(--th-${theme}-eq-press-toggle-bg)`;
-});
-switch_inp.addEventListener("mouseup", function () {
+    case "=":
+      e.target.style.backgroundColor = `var(--th-${theme}-eq-toggle-bg)`;
+      break;
+
+    default:
+      e.target.style.backgroundColor = `var(--th-${theme}-key-bg-main)`;
+      break;
+  }
+};
+const resetSwitchColors = function () {
   switch_inp.firstElementChild.style.backgroundColor = `var(--th-${theme}-eq-toggle-bg)`;
+};
+
+const changeTheme = function (themeNum) {
+  // Main Background
+  document.querySelector(
+    "body"
+  ).style.backgroundColor = `var(--th-${themeNum}-main-bg)`;
+
+  // Header
+  document.querySelector(
+    ".header"
+  ).style.color = `var(--th-${themeNum}-text-input)`;
+
+  // Screen
+  screen_inp.style.color = `var(--th-${themeNum}-text-input)`;
+  screen_inp.style.backgroundColor = `var(--th-${themeNum}-screen-bg)`;
+
+  // Switch Background
+  document.querySelector(
+    ".switch"
+  ).style.backgroundColor = `var(--th-${themeNum}-toggle-bg)`;
+
+  // Switch Toggle
+  document.querySelector(
+    ".toggle"
+  ).style.backgroundColor = `var(--th-${themeNum}-eq-toggle-bg)`;
+
+  // Keyboard Background
+  inputs_container.style.backgroundColor = `var(--th-${themeNum}-toggle-bg)`;
+  // Keys
+  inputs_container.querySelectorAll("button").forEach((button) => {
+    // ${themeNum}
+    if (button.value === "del" || button.value === "reset") {
+      button.style.backgroundColor = `var(--th-${themeNum}-key-bg-alt)`;
+      button.style.boxShadow = `0 5px 0 0 var(--th-${themeNum}-key-sh-alt)`;
+    } else if (button.value === "=") {
+      button.style.backgroundColor = `var(--th-${themeNum}-eq-toggle-bg)`;
+      button.style.boxShadow = `0 5px 0 0 var(--th-${themeNum}-eq-sh)`;
+      button.style.color = `var(--th-${themeNum}-text-eq)`;
+    } else {
+      button.style.backgroundColor = `var(--th-${themeNum}-key-bg-main)`;
+      button.style.boxShadow = `0 5px 0 0 var(--th-${themeNum}-key-sh-main)`;
+      button.style.color = `var(--th-${themeNum}-text)`;
+    }
+  });
+};
+
+const moveToggle = function (themeNum) {
+  const toggle = document.querySelector(".toggle");
+
+  if (themeNum === 1) {
+    toggle.style.left = `calc(0% + 6px)`;
+    toggle.style.transform = `translate(0%, -50%)`;
+  }
+  if (themeNum === 2) {
+    toggle.style.left = `calc(50% + 0px)`;
+    toggle.style.transform = `translate(-50%, -50%)`;
+  }
+  if (themeNum === 3) {
+    toggle.style.left = `calc(100% - 6px)`;
+    toggle.style.transform = `translate(-100%, -50%)`;
+  }
+};
+
+const putInNumber = function (number) {
+  if (screen_inp.value === "0" || operation)
+    (screen_inp.value = number), (operation = false);
+  else screen_inp.value += number;
+};
+
+const resetScreen = function () {
+  screen_inp.value = "0";
+};
+
+const delete1NumberFrom = function () {
+  let inp = screen_inp.value;
+
+  if (inp.length > 1) screen_inp.value = inp.slice(0, inp.length - 1);
+  else screen_inp.value = "0";
+};
+
+const addOperation = function (type) {
+  if (firstNum && !operation) return;
+
+  operation = true;
+  operationType = type;
+
+  firstNum = Number.parseFloat(screen_inp.value);
+};
+
+const getResult = function () {
+  if (!firstNum) return;
+
+  secondNum = Number.parseFloat(screen_inp.value);
+
+  screen_inp.value = calculate(firstNum, operationType, secondNum);
+};
+
+switch_inp.addEventListener("mousedown", function (e) {
+  switch_inp.firstElementChild.style.backgroundColor = `var(--th-${theme}-eq-toggle-press-bg)`;
+});
+switch_inp.addEventListener("mouseup", function (e) {
+  ++theme;
+  if (theme === 4) theme = 1;
+  changeTheme(theme);
+  moveToggle(theme);
+
+  resetSwitchColors();
+});
+switch_inp.addEventListener("mouseout", function (e) {
+  resetSwitchColors();
 });
 
 inputs_container.addEventListener("mousedown", function (e) {
@@ -53,22 +173,6 @@ inputs_container.addEventListener("mousedown", function (e) {
       break;
   }
 });
-const resetColors = function (e) {
-  switch (e.target.value) {
-    case "del":
-    case "reset":
-      e.target.style.backgroundColor = `var(--th-${theme}-key-bg-alt)`;
-      break;
-
-    case "=":
-      e.target.style.backgroundColor = `var(--th-${theme}-eq-toggle-bg)`;
-      break;
-
-    default:
-      e.target.style.backgroundColor = `var(--th-1-key-bg-main)`;
-      break;
-  }
-};
 inputs_container.addEventListener("mouseup", function (e) {
   if (e.target.classList.contains("inputs")) return;
 
@@ -84,24 +188,18 @@ inputs_container.addEventListener("mouseup", function (e) {
     case "8":
     case "9":
     case ".": {
-      if (screen_inp.value === "0" || operation)
-        (screen_inp.value = e.target.value), (operation = false);
-      else screen_inp.value += e.target.value;
+      putInNumber(e.target.value);
       break;
     }
 
     case "reset": {
-      screen_inp.value = "0";
-
+      resetScreen();
       resetNumbers();
       break;
     }
 
     case "del": {
-      let inp = screen_inp.value;
-
-      if (inp.length > 1) screen_inp.value = inp.slice(0, inp.length - 1);
-      else screen_inp.value = "0";
+      delete1NumberFrom();
       break;
     }
 
@@ -109,21 +207,12 @@ inputs_container.addEventListener("mouseup", function (e) {
     case "-":
     case "x":
     case "/": {
-      if (firstNum) break;
-
-      operation = true;
-      operationType = e.target.value;
-
-      firstNum = Number.parseFloat(screen_inp.value);
+      addOperation(e.target.value);
       break;
     }
 
     case "=": {
-      if (!firstNum) break;
-
-      secondNum = Number.parseFloat(screen_inp.value);
-
-      screen_inp.value = calculate(firstNum, operationType, secondNum);
+      getResult();
 
       resetNumbers();
       break;
